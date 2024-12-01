387729d04edba7f5e609b169d0fa35aea023a05a
Dylan Beattie <dylan@dylanbeattie.net>
2024-12-01 18:37:01 +0000

    We can read lists in Rockstar

--- day1/day1/Program.cs
+++ day1/day1/Program.cs
@@ -1,23 +1,22 @@
 ﻿var pairs = File.ReadAllLines("input.txt")
 	.Select(line => line.Split("   "));
 
-var list1 = pairs.Select(pair => Int32.Parse(pair[0])).OrderBy(v => v);
-var list2 = pairs.Select(pair => Int32.Parse(pair[1])).OrderBy(v => v);
+var list1 = pairs.ExtractAndOrderColumn(0);
+var list2 = pairs.ExtractAndOrderColumn(1);
+
 var part1 = list1.Zip(list2, (lhs,  rhs) => Math.Abs(lhs - rhs)).Sum();
 Console.WriteLine($"Solution to part 1: {part1}");
 
-var counts = list2.GroupBy(v => v).ToDictionary(group => group.Key, group => group.Count());
+var counts = list2.GroupBy(v => v)
+	.ToDictionary(group => group.Key, group => group.Count());
 
-var products = list1.Select(n => n * counts.GetValueOrDefault(n));
+var part2 = list1.Select(n => n * counts.GetValueOrDefault(n)).Sum();
 
-var part2 = products.Sum();
 Console.WriteLine($"Solution to part 2: {part2}");
 
-// foreach(var l in list1) Console.WriteLine(l);
-// Console.ReadKey();
-// foreach(var l in list2) Console.WriteLine(l);
-
-
-
+public static class IEnumerableExtensions {
+	public static IEnumerable<int> ExtractAndOrderColumn(this IEnumerable<string[]> list, int colIndex)
+		=> list.Select(cols => Int32.Parse(cols[colIndex])).OrderBy(v => v);
+}
 
 
