56096dc4ef9268fdc48774d4f18bda10b9509083
Dylan Beattie <dylan@dylanbeattie.net>
2024-12-01 19:40:33 +0000

    IT WORKS.

---
+++ day1/day1/groupify.rock.c
@@ -0,0 +1,26 @@
+rock the data with 1, 2, 3, 4, 4, 5, 4, 3, 2, 2, 2, 1, 2, 1, 2, 1 , 2, 1
+
+groupify takes the list
+  rock the counts
+  let the counts at 1 be 888
+  let the counts at 2 be 777
+  for every number in the list
+    if the counts at the number
+      shout "yes"
+      let the value be the counts at the number
+      build the value up
+      shout the number with ": " with the value
+      let the counts at the number be the value
+    otherwise
+      shout "no"
+      let the counts at the number be 1
+    yeah
+  baby
+  debug the counts
+  give back the counts
+baby
+debug the data
+
+let the result be groupify taking the data
+debug the result
+
