8ce76389150a29e581b7a00468e1738eaf6144ad
Dylan Beattie <dylan@dylanbeattie.net>
2024-12-07 11:01:40 +0000

    Eliminate duplication and pay 130ms for the privilege.

--- day7/Program.cs
+++ day7/Program.cs
@@ -1,7 +1,14 @@
-var equations = File.ReadAllLines("example.txt")
+using System.Diagnostics;
+using System.Numerics;
+
+var equations = File.ReadAllLines("input.txt")
 	.Select(line => new Equation(line));
 
-foreach (var eq in equations) Console.WriteLine(eq);
+var sw = new Stopwatch();
+sw.Start();
+var part1 = equations.Where(e => e.Solvable).Sum(e => e.Answer);
+sw.Stop();
+Console.WriteLine($"Part 1: {part1} ({sw.ElapsedMilliseconds} ms)");
 
 public class Equation {
 	public Equation(string line) {
@@ -12,4 +19,21 @@ public class Equation {
 	public long Answer { get; set; }
 	public List<long> Inputs { get; set; }
 	public override string ToString() => $"{Answer}: {String.Join(',', Inputs)}";
+
+	public bool Solvable => Inputs.Solvable(Answer);
 }
+
+public static class ListExtensions {
+	public static IEnumerable<List<T>> Reduce<T>(this List<T> list) where T : INumber<T> {
+		foreach (var result in new[] { list[0] + list[1], list[0] * list[1] }) {
+			yield return ((List<T>) [result]).Concat(list.Skip(2)).ToList();
+		}
+	}
+
+	public static bool Solvable<T>(this List<T> list, T result) where T : INumber<T> {
+		if (list.Count > 1) return list.Reduce().Any(l => l.Solvable(result));
+		return (list[0] == result);
+	}
+}
+
+
