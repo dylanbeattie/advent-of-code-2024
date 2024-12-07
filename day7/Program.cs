689f82cea173ff3d4a324ec825eb53da04c35933
Dylan Beattie <dylan@dylanbeattie.net>
2024-12-07 11:13:33 +0000

    DAY 7: refactor part 1 to take a list of operators

--- day7/Program.cs
+++ day7/Program.cs
@@ -6,7 +6,8 @@ var equations = File.ReadAllLines("input.txt")
 
 var sw = new Stopwatch();
 sw.Start();
-var part1 = equations.Where(e => e.Solvable).Sum(e => e.Answer);
+List<Func<long, long, long>> part1Operators = [(a, b) => a * b, (a, b) => a + b];
+var part1 = equations.Where(e => e.Solvable(part1Operators)).Sum(e => e.Answer);
 sw.Stop();
 Console.WriteLine($"Part 1: {part1} ({sw.ElapsedMilliseconds} ms)");
 
@@ -20,18 +21,21 @@ public class Equation {
 	public List<long> Inputs { get; set; }
 	public override string ToString() => $"{Answer}: {String.Join(',', Inputs)}";
 
-	public bool Solvable => Inputs.Solvable(Answer);
+	public bool Solvable(List<Func<long, long, long>> reductions)
+		=> Inputs.Solvable<long>(Answer, reductions);
 }
 
 public static class ListExtensions {
-	public static IEnumerable<List<T>> Reduce<T>(this List<T> list) where T : INumber<T> {
-		foreach (var result in new[] { list[0] + list[1], list[0] * list[1] }) {
-			yield return ((List<T>) [result]).Concat(list.Skip(2)).ToList();
+	public static IEnumerable<List<T>> Reduce<T>(this List<T> list, List<Func<T, T, T>> reductions)
+		where T : INumber<T> {
+		foreach (var reduce in reductions) {
+			yield return ((T[]) [reduce(list[0], list[1])]).Concat(list.Skip(2)).ToList();
 		}
 	}
 
-	public static bool Solvable<T>(this List<T> list, T result) where T : INumber<T> {
-		if (list.Count > 1) return list.Reduce().Any(l => l.Solvable(result));
+	public static bool Solvable<T>(this List<T> list, T result,
+		List<Func<T,T,T>> reductions) where T : INumber<T> {
+		if (list.Count > 1) return list.Reduce(reductions).Any(l => l.Solvable(result, reductions));
 		return (list[0] == result);
 	}
 }
