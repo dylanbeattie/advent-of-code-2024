d8dcc7e01e715e761772dff05e9390bc838e72ed
Dylan Beattie <dylan@dylanbeattie.net>
2024-12-09 17:04:12 +0000

    DAY 9: first bit of parsing is done

---
+++ day9/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+var input = "2333133121414131402";
+
+var id = 0;
+var map = String.Join("", input.ToCharArray()
+	.Select(c => c - '0')
+	.Chunk(2)
+	.Select(pair =>
+		String.Empty.PadRight(pair[0], (id++).ToString()[0])
+		+
+		(pair.Length > 1 ? String.Empty.PadRight(pair[1], '.') : "")
+	));
+Console.WriteLine(map);
