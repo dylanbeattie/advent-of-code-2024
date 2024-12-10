4f2324f29bf085dc1e738ab697e17cf76263d12a
Dylan Beattie <dylan@dylanbeattie.net>
2024-12-10 17:11:49 +0000

    DAY 10 done! Parts 1 & 2 in C#

--- day10/Program.cs
+++ day10/Program.cs
@@ -1,8 +1,27 @@
-﻿var grid = File.ReadAllLines("example.txt")
-	.Select(line => line.Select(c => (int)c - '0').ToArray())
+﻿using System.Diagnostics;
+
+var grid = File.ReadAllLines("input.txt")
+	.Select(line => line.Select(c => c - '0').ToArray())
 	.ToArray();
 
-foreach(var line in grid) {
-	foreach(var cell in line) Console.Write($"{cell} ");
-	Console.WriteLine();
+List<(int Row, int Col)> trailheads = [];
+for (var row = 0; row < grid.Length; row++) {
+	for (var col = 0; col < grid[0].Length; col++) {
+		if (grid[row][col] == 0) trailheads.Add((row, col));
+	}
 }
+var sw = new Stopwatch();
+sw.Start();
+var part1 = 0;
+foreach(var th in trailheads) {
+	var hash = new HashSet<(int Row, int Col)>();
+	grid.FindPeaks(th.Row, th.Col, hash);
+	part1 += hash.Count;
+}
+Console.WriteLine($"Part 1: {part1} ({sw.ElapsedMilliseconds}ms)");
+sw.Restart();
+var part2 = 0;
+foreach(var th in trailheads) part2 += grid.FindTrails(th.Row, th.Col);
+Console.WriteLine($"Part 2: {part2} ({sw.ElapsedMilliseconds}ms)");
+
+
