eb143399c11917212ddd8450f9f2c8a93ab5a229
Dylan Beattie <dylan@dylanbeattie.net>
2024-12-10 16:16:50 +0000

    DAY 10: parsing grid works

---
+++ day10/Program.cs
@@ -0,0 +1,8 @@
+﻿var grid = File.ReadAllLines("example.txt")
+	.Select(line => line.Select(c => (int)c - '0').ToArray())
+	.ToArray();
+
+foreach(var line in grid) {
+	foreach(var cell in line) Console.Write($"{cell} ");
+	Console.WriteLine();
+}
