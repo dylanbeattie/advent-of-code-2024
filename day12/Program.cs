a993ee5900e99af4d23d8614e1dbd528818974ed
Dylan Beattie <dylan@dylanbeattie.net>
2024-12-12 22:51:02 +0000

    DAY 12 part 2. Preeeetty.

--- day12/Program.cs
+++ day12/Program.cs
@@ -1,6 +1,6 @@
 using System.Runtime.InteropServices;
 
-var grid = File.ReadAllLines("scratch.txt")
+var grid = File.ReadAllLines("input.txt")
 	.Select(line => line.ToCharArray())
 	.ToArray();
 
@@ -10,58 +10,67 @@ var regions = grid.MapRegions();
 var areas = regions.SelectMany(line => line).GroupBy(cell => cell)
 	.ToDictionary(group => group.Key, group => group.Count());
 
-Dictionary<int, int> regionsToPerimeters = new();
+Dictionary<int, int> regionsToPerimeters = [];
 
 for (var row = 0; row < regions.Length; row++) {
 	for (var col = 0; col < regions[row].Length; col++) {
 		var region = regions[row][col];
-		CollectionsMarshal.GetValueRefOrAddDefault(regionsToPerimeters, region, out _)
-			+= perimeterMap[row][col];
+		CollectionsMarshal.GetValueRefOrAddDefault(regionsToPerimeters, region, out _) += perimeterMap[row][col];
 	}
 }
 
 var part1 = regionsToPerimeters.Sum(pair => pair.Value * areas[pair.Key]);
 Console.WriteLine(part1);
 
-Dictionary<int, List<(int,int)>> corners = new();
-for (var row = -1; row < regions.Length; row++) {
-	for (var col = -1; col < regions[0].Length; col++) {
-		var region = regions.At(row,col);
-		corners.TryAdd(region, new());
-		var found = regions.FindCorners(row,col).Distinct();
-		Console.Write(region + ": ");
-		foreach(var finding in found) {
-			Console.WriteLine("   " + finding.row + ", " + finding.col);
-		}
-		corners[region].AddRange();
-	}
-}
-Console.WriteLine(String.Empty.PadRight(72, '-'));
+Dictionary<int,int> corners = [];
 for (var row = 0; row < regions.Length; row++) {
-	for (var col = 0; col < regions[row].Length; col++) {
-		Console.Write(regions[row][col]);
+	for (var col = 0; col < regions[0].Length; col++) {
+		var region = regions[row][col];
+		corners.TryAdd(region, 0);
+		if ("┏┛".Contains(regions.TopLeft(row,col))) corners[region]++;
+		if ("┗┓".Contains(regions.TopRight(row,col))) corners[region]++;
+		if ("┏┛".Contains(regions.BottomRight(row,col))) corners[region]++;
+		if ("┗┓".Contains(regions.BottomLeft(row,col))) corners[region]++;
+	}
+
+	for (var col = 0; col < regions[0].Length; col++) {
+		Console.Write(regions.TopLeft(row, col));
+		var top = regions.Top(row,col);
+		Console.Write($"{top}{top}{top}");
+		var tr = regions.TopRight(row, col);
+		Console.Write(tr);
+		Console.Write(tr == '┓' ? ' ' : (tr == '┗' ? '━' : top));
+	}
+	Console.WriteLine();
+	for (var col = 0; col < regions[0].Length; col++) {
+		Console.Write(regions.Left(row,col));
+		Console.Write(' ');
+		Console.Write(grid[row][col]);
+		Console.Write(' ');
+		Console.Write(regions.Right(row,col));
+		Console.Write(' ');
+	}
+	Console.WriteLine();
+	for (var col = 0; col < regions[0].Length; col++) {
+		Console.Write(regions.BottomLeft(row, col));
+		var bottom = regions.Bottom(row,col);
+		Console.Write($"{bottom}{bottom}{bottom}");
+		var br = regions.BottomRight(row, col);
+		Console.Write(br);
+		Console.Write(br == '┛' ? ' ' : (br == '┏' ? '━' : bottom));
 	}
 	Console.WriteLine();
 }
+Console.WriteLine();
 
-corners.Remove(0);
-foreach(var region in corners.Keys) {
-	Console.Write($"{region}: ");
-	Console.Write(String.Join(", ", corners[region].Distinct().Select(pair => $"({pair.Item1},{pair.Item2})")));
-	Console.WriteLine();
-}
-
+var part2 = corners.Sum(pair => pair.Value * areas[pair.Key]);
+Console.WriteLine(part2);
 
 public static class Extensions {
 	public static int[][] MapRegions(this char[][] grid) {
 		int[][] result = new int[grid.Length][];
-
-		for (int i = 0; i < result.Length; i++) {
-			result[i] = new int[grid[0].Length];
-		}
-
+		for (int i = 0; i < result.Length; i++) result[i] = new int[grid[0].Length];
 		int regionId = 1;
-
 		for (int r = 0; r < grid.Length; r++) {
 			for (int c = 0; c < grid[r].Length; c++) {
 				if (result[r][c] != 0) continue;
@@ -70,45 +79,90 @@ public static class Extensions {
 				regionId++;
 			}
 		}
-
 		return result;
 	}
 
-	public static IEnumerable<(int row, int col)> FindCorners(this int[][] grid, int row, int col) {
-		if (grid.HasCornerAt(row-1,col-1)) yield return(row-1,col-1);
-		if (grid.HasCornerAt(row-1,col)) yield return(row-1,col-1);
-		if (grid.HasCornerAt(row-1,col+1)) yield return(row-1,col-1);
-		if (grid.HasCornerAt(row,col-1)) yield return(row-1,col-1);
-		if (grid.HasCornerAt(row, col)) yield return (row, col);
-		if (grid.HasCornerAt(row, col + 1)) yield return (row, col + 1);
-		if (grid.HasCornerAt(row + 1, col-1)) yield return (row + 1, col);
-		if (grid.HasCornerAt(row + 1, col)) yield return (row + 1, col);
-		if (grid.HasCornerAt(row + 1, col + 1)) yield return (row + 1, col + 1);
+	public static char TopLeft(this int[][] grid, int row, int col) {
+		var nw = grid.At(row - 1, col - 1) == grid.At(row, col);
+		var w = grid.At(row, col - 1) == grid.At(row, col);
+		var n = grid.At(row - 1, col) == grid.At(row, col);
+		if (w && n && !nw) return '┛';
+		if (w && !n) return grid.Top(row,col);
+		if (n && !w) return grid.Left(row,col);
+		if (w && n && nw) return ' ';
+		return '┏';
 	}
 
-	private static bool HasCornerAt(this int[][] grid, int row, int col) {
-		//  Console.WriteLine($"{row}, {col} ===========================");
-		var nw = grid.At(row, col);
-		var ne = grid.At(row, col + 1);
-		var sw = grid.At(row + 1, col);
-		var se = grid.At(row + 1, col + 1);
-		//  Console.WriteLine($"""
-		//      {nw} {ne}
-		//      {sw} {se}
+	public static char TopRight(this int[][] grid, int row, int col) {
+		var ne = grid.At(row - 1, col + 1) == grid.At(row, col);
+		var e = grid.At(row, col + 1) == grid.At(row, col);
+		var n = grid.At(row - 1, col) == grid.At(row, col);
+		if (e && n && !ne) return '┗';
+		if (n && !e) return grid.Right(row,col);
+		if (e && !n) return grid.Top(row,col);
+		if (e && n && ne) return ' ';
+		return '┓';
 
-		//  """);
-
-		var bits = (nw == ne ? 1 : 0, nw == sw ? 1 : 0, nw == se ? 1 : 0);
-		// Console.WriteLine($"{bits.Item1}{bits.Item2}{bits.Item3}");
-		var result =  bits switch {
-			(0, 1, 0) => false,
-			(1, 0, 0) => false,
-			(1, 1, 1) => false,
-			_ => true
-		};
-		// if (result) Console.WriteLine($"corner at {row}, {col}!");
-		return result;
 	}
+
+	public static char BottomLeft(this int[][] grid, int row, int col) {
+		var sw = grid.At(row + 1, col - 1) == grid.At(row, col);
+		var w = grid.At(row, col - 1) == grid.At(row, col);
+		var s = grid.At(row + 1, col) == grid.At(row, col);
+		if (w && s && !sw) return '┓';
+		if (s && !w) return grid.Left(row,col);
+		if (w && !s) return grid.Bottom(row,col);
+		if (w && s && sw) return ' ';
+		return '┗';
+	}
+
+	public static char BottomRight(this int[][] grid, int row, int col) {
+		var se = grid.At(row + 1, col + 1) == grid.At(row, col);
+		var e = grid.At(row, col + 1) == grid.At(row, col);
+		var s = grid.At(row + 1, col) == grid.At(row, col);
+		if (e && s && !se) return '┏';
+		if (e && !s) return grid.Bottom(row,col);
+		if (s && !e) return grid.Right(row,col);
+		if (s && e && se) return ' ';
+		return '┛';
+	}
+
+
+	public static char Left(this int[][] grid, int row, int col) => grid.At(row,col-1) == grid.At(row,col) ? ' ' : '┃';
+	public static char Right(this int[][] grid, int row, int col) => grid.At(row,col+1) == grid.At(row,col) ? ' ' : '┃';
+	public static char Top(this int[][] grid, int row, int col) => grid.At(row-1,col) == grid.At(row,col) ? ' ' : '━';
+	public static char Bottom(this int[][] grid, int row, int col) => grid.At(row+1,col) == grid.At(row,col) ? ' ' : '━';
+
+
+	// public static IEnumerable<(int row, int col)> FindCorners(this int[][] grid, int row, int col) {
+
+
+
+	// }
+
+	// private static bool HasCornerAt(this int[][] grid, int row, int col) {
+	// 	//  Console.WriteLine($"{row}, {col} ===========================");
+	// 	var nw = grid.At(row, col);
+	// 	var ne = grid.At(row, col + 1);
+	// 	var sw = grid.At(row + 1, col);
+	// 	var se = grid.At(row + 1, col + 1);
+	// 	//  Console.WriteLine($"""
+	// 	//      {nw} {ne}
+	// 	//      {sw} {se}
+
+	// 	//  """);
+
+	// 	var bits = (nw == ne ? 1 : 0, nw == sw ? 1 : 0, nw == se ? 1 : 0);
+	// 	// Console.WriteLine($"{bits.Item1}{bits.Item2}{bits.Item3}");
+	// 	var result =  bits switch {
+	// 		(0, 1, 0) => false,
+	// 		(1, 0, 0) => false,
+	// 		(1, 1, 1) => false,
+	// 		_ => true
+	// 	};
+	// 	// if (result) Console.WriteLine($"corner at {row}, {col}!");
+	// 	return result;
+	// }
 
 	public static int At(this int[][] grid, int row, int col) {
 		if (row < 0) return 0;
