17972f4f1aef8e6e19f7c0a6dc63b8a535b40c4f
Dylan Beattie <dylan@dylanbeattie.net>
2024-12-13 18:02:44 +0000

    DAY 13: done!

--- day13/Program.cs
+++ day13/Program.cs
@@ -1,20 +1,38 @@
 ﻿using System.Text.RegularExpressions;
 
-var machines = File.ReadAllText("scratch.txt")
+var machines = File.ReadAllText("input.txt")
 	.Split(Environment.NewLine + Environment.NewLine)
 		.Select(ClawMachine.Parse).ToList();
 
-foreach (var machine in machines) {
-	Console.WriteLine(machine);
-}
+var part1 = machines.Select(m => m.Solve()).Where(cost => cost > 0).Sum();
+Console.WriteLine($"Part 1: {part1}");
 
+const long TEN_TRILLION = 10_000_000_000_000;
 
-public record Button(int X, int Y) {
-}
-public record ClawMachine(Button A, Button B, int X, int Y) {
+var part2 = machines.Select(m => m.Solve(TEN_TRILLION)).Where(cost => cost > 0).Sum();
+Console.WriteLine($"Part 2: {part2}");
+
+public record Button(long X, long Y);
+
+public record ClawMachine(Button A, Button B, long X, long Y) {
+
+	public long Solve(long offset = 0) {
+		decimal ax = A.X;
+		decimal ay = A.Y;
+		decimal bx = B.X;
+		decimal by = B.Y;
+		var n = ((offset + X) * ay - (offset +Y) * ax) / (bx * ay - ax * by);
+		var m = (offset + Y - n * by) / ay;
+		if ((n == (long)n) && (m == (long)m)) return (3 * (long)m) + (long)n;
+		return 0;
+	}
+
 	internal static ClawMachine Parse(string input) {
-		var numbers = input.ParseDigits();
-		if (numbers.Length != 6) throw new Exception("WRONG NUMBERS");
+		var numbers = input.ReplaceLineEndings().Replace(Environment.NewLine, "").ParseDigits();
+		if (numbers.Length != 6) {
+			Console.Write(input);
+			throw new Exception("WRONG NUMBERS");
+		}
 		return new ClawMachine(
 			new Button(numbers[0], numbers[1]),
 			new Button(numbers[2], numbers[3]),
@@ -23,9 +41,9 @@ public record ClawMachine(Button A, Button B, int X, int Y) {
 }
 
 public static class StringExtensions {
-	public static int[] ParseDigits(this string input)
+	public static long[] ParseDigits(this string input)
 		=> Regex.Matches(input, "[0-9]+", RegexOptions.Singleline)
-			.Select(match => Int32.Parse(match.Value)).ToArray();
+			.Select(match => Int64.Parse(match.Value)).ToArray();
 }
 
 
