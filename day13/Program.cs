b60f5ac6af600255f1dd7bee20d1d3fd8f184309
Dylan Beattie <dylan@dylanbeattie.net>
2024-12-13 15:14:19 +0000

    DAY 13 parsing input - done in only 1 hr and 14 minutes! Go chat!

---
+++ day13/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+var machines = File.ReadAllText("scratch.txt")
+	.Split(Environment.NewLine + Environment.NewLine)
+		.Select(ClawMachine.Parse).ToList();
+
+foreach (var machine in machines) {
+	Console.WriteLine(machine);
+}
+
+
+public record Button(int X, int Y) {
+}
+public record ClawMachine(Button A, Button B, int X, int Y) {
+	internal static ClawMachine Parse(string input) {
+		var numbers = input.ParseDigits();
+		if (numbers.Length != 6) throw new Exception("WRONG NUMBERS");
+		return new ClawMachine(
+			new Button(numbers[0], numbers[1]),
+			new Button(numbers[2], numbers[3]),
+			numbers[4], numbers[5]);
+	}
+}
+
+public static class StringExtensions {
+	public static int[] ParseDigits(this string input)
+		=> Regex.Matches(input, "[0-9]+", RegexOptions.Singleline)
+			.Select(match => Int32.Parse(match.Value)).ToArray();
+}
+
+
