d05d29693f2b1b1f5311bd4796818d7eb644ee94
Dylan Beattie <dylan@dylanbeattie.net>
2024-12-29 22:40:34 +0000

    DAY 19 DONE. YEAH.

---
+++ day19/Program.cs
@@ -0,0 +1,17 @@
+﻿var lines = File.ReadAllLines("input.txt");
+var towels = lines[0].Split(", ").ToArray();
+var designs = lines.Skip(2).ToArray();
+
+Dictionary<string,long> memo = new() { { "", 1 } };
+long Combinations(string design, string[] towels) {
+	if (memo.TryGetValue(design, out var result)) return result;
+	result = towels.Where(design.StartsWith).Sum(t => Combinations(design[t.Length..], towels));
+	memo.Add(design, result);
+	return result;
+}
+
+var part1 = designs.Count(d => Combinations(d, towels) > 0);
+Console.WriteLine($"Part 1: {part1}");
+
+var part2 = designs.Sum(d => Combinations(d, towels));
+Console.WriteLine($"Part 2: {part2}");
